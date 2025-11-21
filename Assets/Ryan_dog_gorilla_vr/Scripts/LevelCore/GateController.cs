63ed720f1f0212838fc21dcaec4d5f681742704a
RyanRothweiler <ryanrothweiler@gmail.com>
2025-11-20 21:42:20 -0800

    Add sound for gate opening.

--- Assets/Ryan_dog_gorilla_vr/Scripts/LevelCore/GateController.cs
+++ Assets/Ryan_dog_gorilla_vr/Scripts/LevelCore/GateController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -12,7 +13,14 @@ public class GateController : MonoBehaviour
     private GameObject doorModel;
 
     [SerializeField]
-    private BoxCollider doorCollider;
+    private AudioSource openSound;
+
+    [SerializeField]
+    private BoxCollider doorTrigger;
+
+    private static readonly Vector3 DOOR_OPEN_POS = new Vector3(0.0f, -1.5f, 0.0f);
+
+    private const float DOOR_OPEN_DUR_SEC = 15.0f;
 
     void OnTriggerEnter(Collider coll)
     {
@@ -22,10 +30,33 @@ public class GateController : MonoBehaviour
             if (playerState.ContainsAtleast(requirements))
             {
                 playerState.ConsumeInventory(requirements);
-
-                doorModel.SetActive(false);
-                doorCollider.enabled = false;
+                doorTrigger.enabled = false;
+                StartCoroutine(Open());
             }
         }
     }
+
+    private IEnumerator Open()
+    {
+        openSound.Play();
+
+        Vector3 startPos = doorModel.transform.localPosition;
+        Vector3 endPos = DOOR_OPEN_POS;
+        float t = 0f;
+
+        while (t < DOOR_OPEN_DUR_SEC)
+        {
+            t += Time.deltaTime;
+            float lerp = Mathf.Clamp01(t / DOOR_OPEN_DUR_SEC);
+
+            doorModel.transform.localPosition = Vector3.Lerp(startPos, endPos, lerp);
+
+            yield return null;
+        }
+
+        // Finished opening
+        doorModel.SetActive(false);
+        openSound.Stop();
+    }
+
 }
